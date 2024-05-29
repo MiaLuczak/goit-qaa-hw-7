c8d06f990475d6d09feaa53d835e3013a2775904
Mio Łuczak <mioluczak@gmail.com>
2024-05-29 12:35:21 +0200

    Creating categories querySelector

---
+++ script.js
@@ -0,0 +1,7 @@
+// Exercise 1
+const categories = document.querySelectorAll('ul#categories li.item');
+// const categories = [];
+// getCategories.forEach((element) =>
+//   categories.push(element.querySelectorAll('.item'))
+// );
+console.log(categories);
