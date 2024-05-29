0f5b0bfc9580fa1ea0cefae77bea42b8c136d04f
Mio Łuczak <mioluczak@gmail.com>
2024-05-29 15:24:52 +0200

    Creating an Array of category names

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 // Exercise 1
+//  Part 1
 const categories = document.querySelectorAll('ul#categories li.item');
 const numberOfCategories = categories.length;
 console.log(`Number of categoires: ${numberOfCategories}`);
+
+//  Part 2
+const categoryName = document.querySelectorAll('ul#categories li.item h2');
+const arrayOfCategoryNames = [];
+
+categoryName.forEach((el) => arrayOfCategoryNames.push(el.textContent));
+// // Alternative loop `for ... of ... `
+// for (const x of categoryName) {
+//   arrayOfCategoryNames.push(x.textContent);
+// }
+
+console.log(arrayOfCategoryNames);
