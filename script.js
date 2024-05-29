a325499c25e14ab6ae7a3d675932cd97a30d00b1
Mio Łuczak <mioluczak@gmail.com>
2024-05-29 12:38:58 +0200

    Creating a constant variable for the number of categories; Creating a template string

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 // Exercise 1
 const categories = document.querySelectorAll('ul#categories li.item');
-// const categories = [];
-// getCategories.forEach((element) =>
-//   categories.push(element.querySelectorAll('.item'))
-// );
-console.log(categories);
+const numberOfCategories = categories.length;
+console.log(`Number of categoires: ${numberOfCategories}`);
