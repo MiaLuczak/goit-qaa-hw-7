57f24766ac00dc47b9f9af8535efd5b3da486aa1
Mio Łuczak <mioluczak@gmail.com>
2024-05-31 17:23:33 +0200

    Converting provided url in object property to url without angle brackets

--- script.js
+++ script.js
@@ -55,3 +55,35 @@ ingredients.forEach((igredient) => {
 	listElement.textContent = igredient;
 	list.appendChild(listElement);
 });
+
+// ====== Exercise 3 ======
+// Provided Array:
+const images = [
+	{
+		url: '<https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260>',
+		alt: 'White and Black Long Fur Cat',
+	},
+	{
+		url: '<https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260>',
+		alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+	},
+	{
+		url: '<https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260>',
+		alt: 'Group of Horses Running',
+	},
+];
+
+// const imageDOMArray = [];
+// images.forEach(({ url: src, alt }) => {
+// 	const imageHTMLElement = document.createElement('img');
+// 	imageHTMLElement.setAttribute('src', src);
+// 	imageDOMArray.push(imageHTMLElement);
+// });
+
+images.forEach(({ url: url, alt }) => {
+	let src = '';
+	if (url.startsWith('<') && url.endsWith('>')) {
+		src = url.slice(1, url.length - 1);
+	}
+	console.log(src);
+});
